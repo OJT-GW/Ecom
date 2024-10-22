fb91cf1765d7b36dec2d9ea5f42967d1c189c9ba
19PHP01 <40120293+hieu521@users.noreply.github.com>
2024-10-22 10:15:12 +0700

    [SHOPDUNK-INDEX] Create base page to add footer ,body header by javascript (#1)

---
+++ app.js
@@ -0,0 +1,21 @@
+fetch('header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header').innerHTML = data;
+    })
+    .catch(error => console.log('Lỗi khi tải header:', error));
+
+// Tải footer và chèn vào phần #footer
+fetch('footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('footer').innerHTML = data;
+    })
+    .catch(error => console.log('Lỗi khi tải footer:', error));
+
+fetch('body.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('body').innerHTML = data;
+    })
+    .catch(error => console.log('Lỗi khi tải footer:', error));
