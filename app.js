66a57cd96e78706c888a12b5f6b9f1e2b26f2714
Khoa Nhat <khoapngcd201807@fpt.edu.vn>
2024-10-24 17:20:26 +0700

    feat: [Create footer and provide information to contact]

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ fetch('header.html')
         document.getElementById('header').innerHTML = data;
     })
     .catch(error => console.log('Lỗi khi tải header:', error));
-
 // Tải footer và chèn vào phần #footer
 fetch('footer.html')
     .then(response => response.text())
@@ -12,7 +11,6 @@ fetch('footer.html')
         document.getElementById('footer').innerHTML = data;
     })
     .catch(error => console.log('Lỗi khi tải footer:', error));
-
 fetch('body.html')
     .then(response => response.text())
     .then(data => {
